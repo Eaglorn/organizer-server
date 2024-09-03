fe1e0cd3fbee8d2b226a37f7a369b01297eaf016
Eaglorn <lexus1.45@mail.ru>
2024-09-03 17:01:06 +1000

    update

--- src/controller/controller.module.ts
+++ src/controller/controller.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { VicoController } from './vico.controller'
+import { AdminController } from './admin.controller'
 import { DBServiceModule } from '../db/db.module'
 import { UtilsModule } from '../util/utils.module'
 import { WebsocketsGatewayModule } from '../gateway/websockets.module'
@@ -8,7 +9,7 @@ import { ThrottlerGuard } from '@nestjs/throttler'
 
 @Module({
   imports: [DBServiceModule, UtilsModule, WebsocketsGatewayModule],
-  controllers: [VicoController],
+  controllers: [VicoController, AdminController],
   providers: [
     {
       provide: APP_GUARD,
--- src/db/storage.service.ts
+++ src/db/storage.service.ts
@@ -1,36 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-import { Storage, Prisma } from '@prisma/client';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from './prisma.service'
+import { Storage, Prisma } from '@prisma/client'
 
 @Injectable()
 export class StorageService {
   constructor(private readonly prisma: PrismaService) {}
 
   async one(params: { where?: Prisma.StorageWhereInput }): Promise<Storage> {
-    const { where } = params;
+    const { where } = params
     return this.prisma.storage.findFirst({
       where,
-    });
+    })
   }
 
   async all(): Promise<Storage[]> {
-    return this.prisma.storage.findMany();
+    return this.prisma.storage.findMany()
   }
 
   async create(data: Prisma.StorageCreateInput): Promise<Storage> {
     return this.prisma.storage.create({
       data,
-    });
+    })
   }
 
   async update(params: {
-    where: Prisma.StorageWhereUniqueInput;
-    data: Prisma.StorageUpdateInput;
+    where: Prisma.StorageWhereUniqueInput
+    data: Prisma.StorageUpdateInput
   }): Promise<Storage> {
-    const { data, where } = params;
+    const { data, where } = params
     return this.prisma.storage.update({
       data,
       where,
-    });
+    })
   }
 }
